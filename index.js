c007f0d4d44a52ff7f0d009fb64b141f9a4aec2b
FallenHopes <vlad.serebrow@yandex.ru>
2019-05-29 13:19:48 +0400

    AAAAAA

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port);
 
