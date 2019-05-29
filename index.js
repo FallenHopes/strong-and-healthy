2ab886bd4432c0674c1c2124221007630274d09b
FallenHopes <vlad.serebrow@yandex.ru>
2019-05-29 13:15:31 +0400

    Доработка деплоя

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+const port = process.env.port || 3000;
 
-server.listen(3000);
+server.listen(port);
 
 app.use(express.static('./public'));
 app.get('/', (req, res) => {
