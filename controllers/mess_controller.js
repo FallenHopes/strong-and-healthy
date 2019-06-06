ed881291442e1a45331991ec5ad50e7bad7d6198
FallenHopes <vlad.serebrow@yandex.ru>
2019-06-06 23:40:00 +0400

    lol

--- controllers/mess_controller.js
+++ controllers/mess_controller.js
@@ -1,7 +1,8 @@
 const { messages } = require('../models');
 
 function appendMess(nick, mess, color, date){
-    console.log(nick + " " + mess + " " + color + " " + date);
+    messages.create({nick: nick, mess: mess, date: date, color: color});
+    console.log("Объект в бд создан! Все объекты в бд: " + messages.findAll());
 }
 
 module.exports = {
