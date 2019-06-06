06393f4a7f22a6a4e76477b73a57318acab7a7e4
FallenHopes <vlad.serebrow@yandex.ru>
2019-06-06 22:05:01 +0400

    createTable

--- controllers/mess_controller.js
+++ controllers/mess_controller.js
@@ -3,7 +3,6 @@ const { Messages } = require('../models');
 function appendMess(nick, mess, color, date){
     Messages.create({nick: nick, mess: mess, date: date, color: color});
 }
-
 module.exports = {
     appendMess
 }
