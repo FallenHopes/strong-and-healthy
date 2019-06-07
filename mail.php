d18558e61fc044c6ed0a7933a03a8f49055a8f5e
FallenHopes <vlad.serebrow@yandex.ru>
2019-06-08 03:27:19 +0400

    lol

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $idea = $_POST['idea'];
+    $name = $_GET['name'];
+    $email = $_GET['email'];
+    $idea = $_GET['idea'];
     $subject = '=?UTF-8?B?'.Base64_encode("Идея или жалоба Strong And Healthy").'?=';
     $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
     $success = mail("vlad.serebrow@yandex.ru", $subject, $idea, $headers);
